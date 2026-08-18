d67ebe9b6c0825b77fa0b9ffd7b3ba4b673d8885
KenzoWVY <kenzowendell@gmail.com>
2026-08-18 09:07:19 -0300

    feat(server): add user refresh token to API

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,19 +1,34 @@
 import { Response, Request } from 'express';
+import jwt from 'jsonwebtoken';
 
-import { registerUser, authUser } from '../services/authService.js';
+import { registerUser, authUser, refreshService} from '../services/authServices.js';
 
 export const register = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
-
-    const newUser = await registerUser(email, password);
-
-    res.status(201).json({ newUser });
+    try {
+        const { email, password } = req.body;
+        const newUser = await registerUser(email, password);
+        res.status(201).json({ newUser });
+    } catch (error: any) {
+        res.status(400).json({ error: error.message});
+    }
 }
 
 export const login = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
+        const { user, token, refreshToken } = await authUser(email, password);
+        res.status(200).json({ user, token, refreshToken });
+    } catch (error: any) {
+        res.status(401).json({ error: error.message });
+    }
+}
 
-    const { user, token } = await authUser(email, password);
-
-    res.status(200).json({ user, token });
+export const refresh = async (req: Request, res: Response) => {
+    try {
+        const { refreshToken } = req.body;
+        const newAccessToken = await refreshService(refreshToken);
+        res.status(200).json({ token: newAccessToken });
+    } catch (error: any) {
+        res.status(403).json({ error: error.message });
+    }
 }
